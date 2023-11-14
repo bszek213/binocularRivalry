5e7071d5aeb162cfb7a2b0d7cae2e5c341da005f
bszek213 <bszekely@nevada.unr.edu>
2023-11-13 21:13:08 -0800

    ADD: mixed percepts

--- mixed_time_plot.py
+++ mixed_time_plot.py
@@ -2,15 +2,18 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+"""
+the mean percentage is 2.83%
+"""
 def hist_mixed():
     df = pd.read_csv('mixed_percentage_per_trial.txt', sep='\t', header=None)
+    print(df.mean())
     df.columns = ['mixed_time']
-    sns.histplot(df['mixed_time'], bins=40, kde=True, color='blue')
+    sns.histplot(df['mixed_time'], bins=40, kde=True, color='tab:blue')
     # custom_xticks = range(0, int(max(df['mixed_time'])), 20)  # Adjust range and step size as needed
     # plt.xticks(custom_xticks)
     # plt.title('Distribution of mixed_time')   
-    plt.xlabel('Percentage of Trial with Mixed Percept',fontweight='bold',fontsize=12)
+    plt.xlabel('Percentage of Trial Duration with Mixed Perception',fontweight='bold',fontsize=12)
     plt.ylabel('Count',fontweight='bold',fontsize=12)
     plt.xticks(weight='bold')
     plt.yticks(weight='bold')
